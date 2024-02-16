126a1caa3b582649d824c5c41d3eec1d82953410
Henry Hollis <henryhollis4@gmail.com>
2024-02-16 15:02:29 -0500

    Using modified libleidenalg, instead of significantly different leiden_ccd as before

--- scripts/build_libleidenalg.sh
+++ scripts/build_libleidenalg.sh
@@ -1,4 +1,4 @@
-leiden_ccd_VERSION=0.11.0
+LIBLEIDENALG_VERSION=0
 
 ROOT_DIR=`pwd`
 echo "Using root dir ${ROOT_DIR}"
@@ -11,44 +11,42 @@ if [ ! -d "${ROOT_DIR}/build-deps/src" ]; then
 fi
 
 cd ${ROOT_DIR}/build-deps/src
-if [ ! -d "leiden_ccd" ]; then
+if [ ! -d "libleidenalg" ]; then
   echo ""
-  echo "Cloning leiden_ccd into ${ROOT_DIR}/build-deps/src/leiden_ccd"
+  echo "Cloning libleidenalg into ${ROOT_DIR}/build-deps/src/libleidenalg"
   # Clone repository if it does not exist yet
-  # git clone --branch ${leiden_ccd_VERSION} https://github.com/HenryHollis/leiden_ccd.git --single-branch
-    git clone https://github.com/HenryHollis/leiden_ccd.git --single-branch
-
+  git clone --branch ${LIBLEIDENALG_VERSION} https://github.com/HenryHollis/libleidenalg.git --single-branch
 fi
 
-# # Make sure the git repository points to the correct version
-# echo ""
-# echo "Checking out ${leiden_ccd_VERSION} in ${ROOT_DIR}/build-deps/src/leiden_ccd"
-# cd ${ROOT_DIR}/build-deps/src/leiden_ccd
-# git fetch origin tag ${leiden_ccd_VERSION} --no-tags
-# git checkout ${leiden_ccd_VERSION}
+# Make sure the git repository points to the correct version
+echo ""
+echo "Checking out ${LIBLEIDENALG_VERSION} in ${ROOT_DIR}/build-deps/src/libleidenalg"
+cd ${ROOT_DIR}/build-deps/src/libleidenalg
+git fetch origin tag ${LIBLEIDENALG_VERSION} --no-tags
+git checkout ${LIBLEIDENALG_VERSION}
 
 # Make build directory
-if [ ! -d "${ROOT_DIR}/build-deps/build/leiden_ccd" ]; then
+if [ ! -d "${ROOT_DIR}/build-deps/build/libleidenalg" ]; then
   echo ""
-  echo "Make directory ${ROOT_DIR}/build-deps/build/leiden_ccd"
-  mkdir -p ${ROOT_DIR}/build-deps/build/leiden_ccd
+  echo "Make directory ${ROOT_DIR}/build-deps/build/libleidenalg"
+  mkdir -p ${ROOT_DIR}/build-deps/build/libleidenalg
 fi
 
 # Configure, build and install
-cd ${ROOT_DIR}/build-deps/build/leiden_ccd
+cd ${ROOT_DIR}/build-deps/build/libleidenalg
 
 echo ""
-echo "Configure leiden_ccd build"
-cmake ${ROOT_DIR}/build-deps/src/leiden_ccd \
+echo "Configure libleidenalg build"
+cmake ${ROOT_DIR}/build-deps/src/libleidenalg \
     -DCMAKE_INSTALL_PREFIX=${ROOT_DIR}/build-deps/install/ \
     -DBUILD_SHARED_LIBS=ON \
     -Digraph_ROOT=${ROOT_DIR}/build-deps/install/lib/cmake/igraph/ \
     ${EXTRA_CMAKE_ARGS}
 
 echo ""
-echo "Build leiden_ccd"
+echo "Build libleidenalg"
 cmake --build .
 
 echo ""
-echo "Install leiden_ccd to ${ROOT_DIR}/build-deps/install/"
+echo "Install libleidenalg to ${ROOT_DIR}/build-deps/install/"
 cmake --build . --target install
