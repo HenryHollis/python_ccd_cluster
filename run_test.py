5691cc279cd1c1217869ee04837c93e7d0cd6ddf
henryhollis <henryhollis4@gmail.com>
2024-03-06 15:55:13 -0500

    Added some code for debugging -  I need to cache the ccd somehow

--- run_test.py
+++ run_test.py
@@ -7,14 +7,15 @@ import random
 from sklearn.metrics.cluster import adjusted_rand_score
 random.seed(42) #in this case only needed so that Erdos_renyi graph stays const
 np.random.seed(42)
-emat  = np.random.rand(12,100)
+emat  = np.random.rand(12,5)
 
 # Calculate the correlation matrix
 correlation_matrix = np.corrcoef(emat.T, rowvar=False)
 ccd = leidenalg.calcCCD(correlation_matrix, emat)
 print("CCD: {:.6f}".format(ccd))
 
-G = ig.Graph.Erdos_Renyi(100, 0.01)
+G = ig.Graph(n=5, edges=[[0, 1], [1,2], [2,3],[3,4], [4,0], [4,1], [4,2]])
+
 part = leidenalg.find_partition(G, leidenalg.ccdModularityVertexPartition, emat, correlation_matrix, seed = 42)
 part2 = leidenalg.find_partition(G, leidenalg.ModularityVertexPartition, seed = 42)
 # part3 = leidenalg.find_partition(G, leidenalg.ModularityVertexPartition, seed = 42)
